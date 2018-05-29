7a72f96f1a4127b25fafa5a0ffafde78ffd6a09a
Danil Semelenov <mail@danil.mobi>
2018-05-30 01:20:41 +0300

    Rename a module

--- cli.js
+++ cli.js
@@ -1,2 +1,2 @@
 #!/usr/bin/env node
-require('./prettier')('bin-prettier');
+require('./require')('bin-prettier');
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = {
-  ...require('./prettier')(),
+  ...require('./require')(),
   languages: [],
 };
