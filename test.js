8856a4cff94e1cb78524fd36b40f8dc3c4868036
Danil Semelenov <mail@danil.mobi>
2018-05-29 18:51:28 +0300

    Add a temporary test file

---
+++ test.js
@@ -0,0 +1,23 @@
+const template = html`<span class="bar">qux {1+1} {1</span>`;
+const template = html`<foo></foo><bar></bar>`;
+const template = html`<div data-foo="bar">${items.map(item => html`<span class="foo bar baz test foo foo foo" foo="qux">${item.id}</span>`)}</div>`;
+const template = html`<span class="foo" hidden>${item.id}</span>`;
+const template = html`<span class="bar ${'foo'+'bar'}" data-foo=${'bar'} style=${{width: 100}}>${'abc' + 'bar' + 'baz' +'foo' + 'bar' + 'baz' + 'foo' + 'bar' + 'baz' + 'foo' + 'bar' + 'baz'}</span>`;
+function foo() {
+  function foo() {
+    function foo() {
+      function foo() {
+        function foo() {
+          const template = html`<span class="bar">qux</span>`;
+          const template = html`<span class="bar">${'qux'}</span>`;
+          const template = html`<div class="foo" data-id="foo" hidden=${false}><span class="bar" data-id="bar" style=${{width: 100, foo: 'bar', baz: 'qux', foo: 'bar', baz: 'qux'}}>${'test'}</span></div>`;
+          const template = html`<span class="foo bar baz test foo foo foo" foo="qux">${item.id}</span>`
+          const template = html`<div data-foo="bar">${items.map(item => html`<span class="foo bar baz test foo foo foo" foo="qux">${item.id}</span>`)}</div>`;
+        }
+      }
+    }
+  }
+}
+
+//const template = html`<style>:root{color:red}</style>`;
+//const template = html`<style>:root{color:${'red'}}</style>`;
