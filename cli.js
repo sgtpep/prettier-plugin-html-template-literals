1f357a0abafafdb0a961e508f8e20532b44f15c0
Danil Semelenov <mail@danil.mobi>
2018-05-29 18:22:22 +0300

    Patch require()

--- cli.js
+++ cli.js
@@ -1,2 +1,15 @@
 #!/usr/bin/env node
-console.log('TODO');
+const Module = require('module');
+
+const _compile = Module.prototype._compile;
+Module.prototype._compile = function(content, path) {
+  const patchedContent = path.endsWith('/prettier/bin-prettier.js')
+    ? content.replace(
+        /\bswitch\s*\(node\.type\)\s*{\n\s*case\s['"]TemplateLiteral['"]:\n/,
+        '$&console.log("TODO")\n'
+      )
+    : content;
+  return _compile.call(this, patchedContent, path);
+};
+
+require('prettier/bin-prettier');
