a62d5cd148a4e6fb97d5c0b1376851e3dca56978
Danil Semelenov <mail@danil.mobi>
2018-05-30 23:43:34 +0300

    Review regexps, process doc's expandedStates recursively

--- embed.js
+++ embed.js
@@ -10,53 +10,58 @@ module.exports = function embed(path, print, textToDoc) {
         ''
       );
     if (/<\/|\/>/.test(text)) {
-      const expressions = node.expressions
-        ? path.map(print, 'expressions')
-        : [];
-      const doc = textToDoc(`<>${text}</>`, { parser: 'babylon' });
-      const processedDoc = mapDoc(doc, doc => {
-        if (doc && doc.parts) {
-          if (doc.parts[0] === '{' && doc.parts[doc.parts.length - 1] === '}') {
-            let match;
-            try {
-              match = doc.parts[1].contents.parts[1].parts[0].match(
-                /@prettier-placeholder-(\d+)/
-              );
-            } catch (error) {}
-            if (match) {
-              return concat(['${', expressions[match[1]], '}']);
-            }
-          } else {
-            for (const [index, part] of doc.parts.entries()) {
-              if (part.includes) {
-                if (part.includes("{'@prettier-placeholder-")) {
-                  const parts = [];
-                  const regExp = /{'@prettier-placeholder-(\d+)-id'}/g;
-                  let match;
-                  let offset = 0;
-                  while ((match = regExp.exec(part))) {
-                    parts.push(
-                      part.slice(offset, match.index),
-                      '${',
-                      expressions[match[1]],
-                      '}'
+      function processDoc(doc) {
+        if (doc) {
+          if (doc.expandedStates) {
+            doc.expandedStates = doc.expandedStates.map(doc =>
+              mapDoc(doc, doc => processDoc(doc))
+            );
+          } else if (doc.parts) {
+            if (
+              doc.parts[0] === '{' &&
+              doc.parts[doc.parts.length - 1] === '}'
+            ) {
+              let match;
+              try {
+                match = doc.parts[1].contents.parts[1].parts[0].match(
+                  /^['"]@prettier-placeholder-(\d+)-id['"]$/
+                );
+              } catch (error) {}
+              if (match) {
+                return concat(['${', expressions[match[1]], '}']);
+              }
+            } else {
+              for (const [index, part] of doc.parts.entries()) {
+                if (part.includes) {
+                  if (part.includes('@prettier-placeholder-')) {
+                    const parts = [];
+                    const regExp = /{['"]@prettier-placeholder-(\d+)-id['"]}/g;
+                    let match;
+                    let offset = 0;
+                    while ((match = regExp.exec(part))) {
+                      parts.push(
+                        part.slice(offset, match.index),
+                        '${',
+                        expressions[match[1]],
+                        '}'
+                      );
+                      offset = match.index + match[0].length;
+                    }
+                    parts.push(part.slice(offset));
+                    doc.parts.splice(index, 1, ...parts);
+                  } else if (part.includes('@prettier-curly-brace')) {
+                    doc.parts[index] = part.replace(
+                      /@prettier-curly-brace/g,
+                      '{'
                     );
-                    offset = match.index + match[0].length;
                   }
-                  parts.push(part.slice(offset));
-                  doc.parts.splice(index, 1, ...parts);
-                } else if (part.includes('@prettier-curly-brace')) {
-                  doc.parts[index] = part.replace(
-                    /@prettier-curly-brace/g,
-                    '{'
-                  );
                 }
               }
             }
           }
         }
         return doc;
-      });
+      }
       function trimDoc(doc) {
         let trimmedDoc = doc;
         while (!trimmedDoc.parts.includes(';')) {
@@ -65,9 +70,16 @@ module.exports = function embed(path, print, textToDoc) {
         return trimmedDoc.parts[0].parts[0].contents.parts[1].contents.parts[1]
           .contents;
       }
-      const trimmedDoc = trimDoc(processedDoc);
-      const line = willBreak(trimmedDoc) ? hardline : '';
-      return concat(['`', indent(concat([line, trimmedDoc])), line, '`']);
+      const expressions = node.expressions
+        ? path.map(print, 'expressions')
+        : [];
+      const doc = trimDoc(
+        mapDoc(textToDoc(`<>${text}</>`, { parser: 'babylon' }), doc =>
+          processDoc(doc)
+        )
+      );
+      const line = willBreak(doc) ? hardline : '';
+      return concat(['`', indent(concat([line, doc])), line, '`']);
     }
   }
 };
