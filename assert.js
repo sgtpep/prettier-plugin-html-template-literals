c35195c9139bcbfd4c25294ff3a6a5e8589d0c6a
Danil Semelenov <mail@danil.mobi>
2018-05-31 01:06:57 +0300

    Use the prettier config in tests

--- assert.js
+++ assert.js
@@ -1,6 +1,11 @@
-const { equal } = require('assert').strict;
-const { format } = require('./prettier');
+const assert = require('assert').strict;
+const prettier = require('./prettier');
 
-module.exports = function assert(actual, expected) {
-  return equal(format(actual, { parser: 'babylon' }), expected);
+const options = prettier.resolveConfig.sync(__filename);
+
+module.exports = function(actual, expected) {
+  return assert.equal(
+    prettier.format(actual, { ...options, parser: 'babylon' }),
+    expected
+  );
 };
