739681ee33787d1d2caf27757326cff7c669b2dd
Danil Semelenov <mail@danil.mobi>
2018-05-31 01:06:36 +0300

    Implement a test case

--- tests/style-attribute-object-value.js
+++ tests/style-attribute-object-value.js
@@ -1,8 +1,12 @@
-//require('../assert')(
-//  "\
-//html`<div style=${{ foo: 'foo', bar: 'bar', baz: 'baz', qux: 'qux' }}>foo</div>`;\n\
-//",
-//  '\
-//html``;\n\
-//'
-//);
+require('../assert')(
+  "\
+html`<div style=${{ foo: 'foo', bar: 'bar', baz: 'baz', qux: 'qux' }}>foo</div>`;\n\
+",
+  '\
+html`\n\
+  <div style=${{ foo: "foo", bar: "bar", baz: "baz", qux: "qux" }}>\n\
+    foo\n\
+  </div>\n\
+`;\n\
+'
+);
