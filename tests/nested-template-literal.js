4951f936a0b9937cac03d66ef8026398491dada2
Danil Semelenov <mail@danil.mobi>
2018-05-31 02:30:24 +0300

    Add more test cases

--- tests/nested-template-literal.js
+++ tests/nested-template-literal.js
@@ -6,3 +6,31 @@ html`<div>${items.map(item => html`<span/>`)}</div>`;\n\
 html`<div>${items.map(item => html`<span />`)}</div>`;\n\
 '
 );
+
+require('../assert')(
+  '\
+html`<div data-foo="foo" data-bar="bar" data-baz="baz">${items.map(item => html`<span/>`)}</div>`;\n\
+',
+  '\
+html`\n\
+  <div data-foo="foo" data-bar="bar" data-baz="baz">\n\
+    ${items.map(item => html`<span />`)}\n\
+  </div>\n\
+`;\n\
+'
+);
+
+require('../assert')(
+  '\
+html`<div>${items.map(item => html`<span data-foo="foo" data-bar="bar" data-baz="baz"/>`)}</div>`;\n\
+',
+  '\
+html`\n\
+  <div>\n\
+    ${items.map(\n\
+      item => html`<span data-foo="foo" data-bar="bar" data-baz="baz" />`\n\
+    )}\n\
+  </div>\n\
+`;\n\
+'
+);
