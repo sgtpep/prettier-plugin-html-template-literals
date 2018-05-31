d44822d263fe3a102ef84d93ffcde3655d05bda7
Danil Semelenov <mail@danil.mobi>
2018-05-31 17:54:12 +0300

    Add a test case

--- tests/style-element.js
+++ tests/style-element.js
@@ -1,6 +1,6 @@
 //require('../assert')(
 //  '\
-//html`<style>:root{color:red}</style >`\n\
+//html`<style>.foo{color:red}</style >`\n\
 //',
 //  '\
 //html``;\n\
@@ -9,8 +9,20 @@
 
 //require('../assert')(
 //  "\
-//html`<style>:root{color:${'red'}}</style >`\n\
+//html`<style>.foo{color:${'red'}}</style >`\n\
 //",
+//  '\
+//html``;\n\
+//'
+//);
+
+//require('../assert')(
+//  '\
+//html`<style>.foo{color:red}\n\
+//.bar{\n\
+//color:blue\n\
+//}</style >`\n\
+//',
 //  '\
 //html``;\n\
 //'
