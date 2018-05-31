c2bdf7ad59912c008a1774135ef97b3568bc8b64
Danil Semelenov <mail@danil.mobi>
2018-05-31 12:01:40 +0300

    Add a test

---
+++ tests/ignore-not-html.js
@@ -0,0 +1,17 @@
+require('../assert')('\
+html``;\n\
+', '\
+html``;\n\
+');
+
+require('../assert')('\
+html`text <>`;\n\
+', '\
+html`text <>`;\n\
+');
+
+require('../assert')('\
+html`<div`;\n\
+', '\
+html`<div`;\n\
+');
