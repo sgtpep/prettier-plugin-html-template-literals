2c9fdca96b159a1e7628d1c4caeae85454f22d87
Danil Semelenov <mail@danil.mobi>
2018-05-31 22:51:45 +0300

    Exit with error code on a failed assertion

--- assert.js
+++ assert.js
@@ -2,10 +2,18 @@ const path = require('path');
 const prettier = require('./prettier');
 
 const options = prettier.resolveConfig.sync(__filename);
+let error;
+
+process.on('exit', code => {
+  if (error && !code) {
+    process.exitCode = 1;
+  }
+});
 
 module.exports = function(source, expected) {
   const formatted = prettier.format(source, { ...options, parser: 'babylon' });
   if (formatted !== expected) {
+    error = true;
     // eslint-disable-next-line no-console
     console.error(`Failed ${path.basename(module.parent.filename)}
 
