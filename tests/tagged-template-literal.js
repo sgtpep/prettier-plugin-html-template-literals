34e0b0fe4a34df67e057478de8c301f5272c5bc8
Danil Semelenov <mail@danil.mobi>
2018-05-31 11:45:56 +0300

    Add test cases

--- tests/tagged-template-literal.js
+++ tests/tagged-template-literal.js
@@ -3,3 +3,21 @@ html`< div/>`;\n\
 ', '\
 html`<div />`;\n\
 ');
+
+require('../assert')('\
+`< div/>`;\n\
+', '\
+`< div/>`;\n\
+');
+
+require('../assert')('\
+foo`< div/>`;\n\
+', '\
+foo`<div />`;\n\
+');
+
+require('../assert')('\
+foo()`< div/>`;\n\
+', '\
+foo()`<div />`;\n\
+');
